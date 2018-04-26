d9a8557983af680d138ac0a55b4cbe201e4f8b25
taihelsel <tai.bayard@gmail.com>
2018-04-26 15:23:26 -0700

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <section id="Header" >
+            Header
+        </section>
+    );
+}
+
+export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+/*React Components*/
+import Header from "./Header.js";
+
 class Home extends Component {
   render() {
     return (
         <section id="Home" >
+            <Header />
             Home page
         </section>
     );
