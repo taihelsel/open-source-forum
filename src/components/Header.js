6fc5e976a2e12611b9656225f8cbe03a45c1236d
taihelsel <tai.bayard@gmail.com>
2018-04-26 23:57:59 -0700

    added placeholder for header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
     return (
         <section id="Header" >
-            Header
+            <h1 className="header-placeholder">Your Logo Here</h1>
+            <nav className="header-nav">
+            </nav>
         </section>
     );
 }
