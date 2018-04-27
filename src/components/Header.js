abe8f58050b63a165f2a25c22232d09f68d81cca
taihelsel <tai.bayard@gmail.com>
2018-04-27 01:41:26 -0700

    added animated hamburger button to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const Header = () => {
-    return (
-        <section id="Header" >
-            <h1 className="header-placeholder">Your Logo Here</h1>
-            <nav className="header-nav">
-                <ul className="social-links-wrapper">
-                    <li className="social-link-item">
-                        <a href="https://www.facebook.com/" target="_blank">
-                            <img src={require(".././media/images/icons/facebook-icon.png")} alt="facebook icon"/>
-                        </a>
-                    </li>
-                    <li className="social-link-item">
-                        <a href="https://www.twitter.com/" target="_blank">
-                            <img src={require(".././media/images/icons/twitter-icon.png")} alt="twitter icon"/>
-                        </a>
-                    </li>
-                    <li className="social-link-item">
-                        <a href="https://www.instagram.com/" target="_blank">
-                            <img src={require(".././media/images/icons/instagram-icon.png")} alt="instagram icon"/>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </section>
-    );
+class Header extends Component {
+    handleHamburgerClick = (e) =>{
+        if(e.currentTarget.classList.contains("hamburger-btn-x")===true){
+            //Close hamburger menu & revert to original state
+            e.currentTarget.classList.remove("hamburger-btn-x");
+            
+        }else{
+            //Open hamburger menu and convert item to X
+            e.currentTarget.classList.add("hamburger-btn-x");
+        }
+    }
+    render() {
+        return (
+            <section id="Header" >
+                <h1 className="header-placeholder">Your Logo Here</h1>
+                <nav className="header-nav">
+                    <ul className="social-links-wrapper">
+                        <li className="social-link-item">
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <img src={require(".././media/images/icons/facebook-icon.png")} alt="facebook icon" />
+                            </a>
+                        </li>
+                        <li className="social-link-item">
+                            <a href="https://www.twitter.com/" target="_blank">
+                                <img src={require(".././media/images/icons/twitter-icon.png")} alt="twitter icon" />
+                            </a>
+                        </li>
+                        <li className="social-link-item">
+                            <a href="https://www.instagram.com/" target="_blank">
+                                <img src={require(".././media/images/icons/instagram-icon.png")} alt="instagram icon" />
+                            </a>
+                        </li>
+                    </ul>
+                    <ul className="nav-control-wrapper">
+                        <li onClick={this.handleHamburgerClick} className="hamburger-btn">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        );
+    }
 }
 
 export default Header;
