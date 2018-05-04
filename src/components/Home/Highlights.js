3cbfe803cf6043b956812cba3bef541a1e765f88
taihelsel <tai.bayard@gmail.com>
2018-05-04 16:51:53 -0700

    added two additional posts to highlights

--- src/components/Home/Highlights.js
+++ src/components/Home/Highlights.js
@@ -4,16 +4,26 @@ class Highlights extends Component {
   render() {
     return (
         <section className="highlights">
-          <ul className="highlight-reel">
-            <li className="highlight-item">
-              <ul className="highlight-desc">
+          <div className="highlight-reel">
+            <div className="main-highlight-item">
+              <div className="main-highlight-desc">
                 <li><h1>Cathedral of Learning</h1></li>
-                <li><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore ducimus nesciunt nisi adipisci. Exercitationem magni fugit, voluptas doloribus ex harum hic sunt at impedit repudiandae laudantium necessitatibus porro molestiae nihil. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos dolorem itaque, possimus quaerat aspernatur tempora atque accusantium, temporibus aut omnis harum pariatur ipsa odit earum, quod dolores culpa dolor laborum?</p></li>
-                <li><button>Keep Reading</button></li>
-              </ul>
+                <li><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore ducimus nesciunt nisi adipisci. Exercitationem magni fugit, voluptas doloribus ex harum hic sunt at impedit repudiandae laudantium necessitatibus porro molestiae nihil. Lorem ipsum...</p></li>
+                <button>Keep Reading</button>
+              </div>
               <img src={require("../.././media/images/stockphotos/ancient-architecture.jpg")} alt="Street view"/>
-            </li>
-          </ul>
+            </div>
+            <ul className="secondary-highlights">
+              <li className="secondary-highlight-item">
+                <h1>Street Ipsum</h1>
+                <img src={require("../.././media/images/stockphotos/street.jpg")} alt="Picture of street"/>
+              </li>
+              <li className="secondary-highlight-item">
+                <h1>Camera Ipsum</h1>
+                <img src={require("../.././media/images/stockphotos/camera.jpg")} alt="Picture of street"/>
+              </li>
+            </ul>
+          </div>
         </section>
     );
   }
