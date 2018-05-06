5cfe4cd032d269608c6b6ad0581d0696762d1317
taihelsel <tai.bayard@gmail.com>
2018-05-05 20:48:28 -0700

    stubbed out latest items

--- src/components/SinglePost/SideBar/Explorer/Latest.js
+++ src/components/SinglePost/SideBar/Explorer/Latest.js
@@ -3,12 +3,43 @@ import React, { Component } from 'react';
 class Latest extends Component {
   render() {
     return (
-        <ul id="explorer-latest-container">
-          <li className="explorer-latest-item">
-              <img src={require("../../../.././media/images/stockphotos/calamity.jpg")}/>
-              <h1>Lorem Ipsum</h1>
-          </li>
-        </ul>
+      <ul id="explorer-latest-container">
+        <li className="explorer-latest-item">
+          <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
+          <h1>Lorem Ipsum</h1>
+          <div className="explorer-latest-item-stats">
+            TODO date posted, views, comments.
+              </div>
+        </li>
+        <li className="explorer-latest-item">
+          <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
+          <h1>Lorem Ipsum</h1>
+          <div className="explorer-latest-item-stats">
+            TODO date posted, views, comments.
+              </div>
+        </li>
+        <li className="explorer-latest-item">
+          <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
+          <h1>Lorem Ipsum</h1>
+          <div className="explorer-latest-item-stats">
+            TODO date posted, views, comments.
+              </div>
+        </li>
+        <li className="explorer-latest-item">
+          <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
+          <h1>Lorem Ipsum</h1>
+          <div className="explorer-latest-item-stats">
+            TODO date posted, views, comments.
+              </div>
+        </li>
+        <li className="explorer-latest-item">
+          <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
+          <h1>Lorem Ipsum</h1>
+          <div className="explorer-latest-item-stats">
+            TODO date posted, views, comments.
+              </div>
+        </li>
+      </ul>
     );
   }
 }
