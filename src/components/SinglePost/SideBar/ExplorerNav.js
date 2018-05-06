77ca62ed5b9eeaf64921b575c3184b1e8299e2b8
taihelsel <tai.bayard@gmail.com>
2018-05-05 19:29:54 -0700

    able to switch between sidebar explorer tabs

--- src/components/SinglePost/SideBar/ExplorerNav.js
+++ src/components/SinglePost/SideBar/ExplorerNav.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
 class ExplorerNav extends Component {
-  render() {
-    return (
-        <div id="explorer-nav">
-            <ul>
-                <li className="selected">Latest</li>
-                <li>Popular</li>
-                <li>Tags</li>
-            </ul>
-        </div>
-    );
-  }
+    handleNavClick = (e) =>{
+        if(!(e.currentTarget.classList.contains("selected"))){
+            document.getElementById("explorer-nav").getElementsByClassName("selected")[0].classList.remove("selected");
+            e.currentTarget.classList.add("selected");
+        }
+    }
+    render() {
+        return (
+            <div id="explorer-nav">
+                <ul>
+                    <li onClick={this.handleNavClick} className="selected">Latest</li>
+                    <li onClick={this.handleNavClick}>Popular</li>
+                    <li onClick={this.handleNavClick}>Tags</li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default ExplorerNav;
