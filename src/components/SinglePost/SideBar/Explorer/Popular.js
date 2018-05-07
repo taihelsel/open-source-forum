2cb6ec3cf3735009ba24ce733b14c0f3b7d1dab9
taihelsel <tai.bayard@gmail.com>
2018-05-06 19:20:16 -0700

    added filler items for popular component

--- src/components/SinglePost/SideBar/Explorer/Popular.js
+++ src/components/SinglePost/SideBar/Explorer/Popular.js
@@ -6,6 +6,18 @@ class Popular extends Component {
             <ul id="explorer-popular-container">
                 <li className="explorer-popular-item">
                     <img src={require("../../../.././media/images/stockphotos/ancient-architecture.jpg")} />
+                    <h1>Cathedral of Learning</h1>
+                    <div className="mask"></div>
+                </li>
+                <li className="explorer-popular-item">
+                    <img src={require("../../../.././media/images/stockphotos/camera.jpg")} />
+                    <h1>Camera Ipsum</h1>
+                    <div className="mask"></div>
+                </li>
+                <li className="explorer-popular-item">
+                    <img src={require("../../../.././media/images/stockphotos/street.jpg")} />
+                    <h1>Street View</h1>
+                    <div className="mask"></div>
                 </li>
             </ul>
         );
