ecc86872275961cdbd399a9fc4e0da0bba63b4b9
taihelsel <tai.bayard@gmail.com>
2018-05-06 19:11:06 -0700

    rendering image in popular component

--- src/components/SinglePost/SideBar/Explorer/Popular.js
+++ src/components/SinglePost/SideBar/Explorer/Popular.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 class Popular extends Component {
-  render() {
-    return (
-      <ul id="explorer-popular-container">
-      popular
-      </ul>
-    );
-  }
+    render() {
+        return (
+            <ul id="explorer-popular-container">
+                <li className="explorer-popular-item">
+                    <img src={require("../../../.././media/images/stockphotos/ancient-architecture.jpg")} />
+                </li>
+            </ul>
+        );
+    }
 }
 
 export default Popular;
