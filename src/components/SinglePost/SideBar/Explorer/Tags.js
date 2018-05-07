40d80b4b7efdeffaa6715318a2fe267434ca4491
taihelsel <tai.bayard@gmail.com>
2018-05-06 19:35:31 -0700

    added tags component filler and style

--- src/components/SinglePost/SideBar/Explorer/Tags.js
+++ src/components/SinglePost/SideBar/Explorer/Tags.js
@@ -4,7 +4,14 @@ class Tags extends Component {
     render() {
         return (
             <ul id="explorer-tags-container">
-            tags
+                <li className="explorer-tags-item">Lorem</li>
+                <li className="explorer-tags-item">News</li>
+                <li className="explorer-tags-item">Ipsum</li>
+                <li className="explorer-tags-item">Camera</li>
+                <li className="explorer-tags-item">Stuff</li>
+                <li className="explorer-tags-item">Raft</li>
+                <li className="explorer-tags-item">Car</li>
+                <li className="explorer-tags-item">Pink</li>
             </ul>
         );
     }
