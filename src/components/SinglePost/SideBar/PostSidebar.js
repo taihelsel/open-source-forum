ca13cf4e5f183b8069ebd9b79f775d48d20fbdfc
taihelsel <tai.bayard@gmail.com>
2018-05-07 23:38:34 -0700

    stubbed out search box for sidebar

--- src/components/SinglePost/SideBar/PostSidebar.js
+++ src/components/SinglePost/SideBar/PostSidebar.js
@@ -5,6 +5,7 @@ class PostSidebar extends Component {
   render() {
     return (
         <div id="PostSidebar">
+          <input className="sidebar-search" type="text" placeholder="Search"/>
           <Explorer />
         </div>
     );
