57564cf370151497f2eaf8e9b8fa23f2734e6e30
taihelsel <tai.bayard@gmail.com>
2018-05-08 02:22:39 -0700

    added social images to author about

--- src/components/SinglePost/Body/AuthorAbout.js
+++ src/components/SinglePost/Body/AuthorAbout.js
@@ -6,6 +6,11 @@ class AuthorAbout extends Component {
         <div id="about-author">
           <img className="author-headshot" src={require("../../.././media/images/stockphotos/headshot.jpg")}/>
           <h1 className="author-name">Jane Doe</h1>
+          <ul className="author-social">
+          <li><img src={require("../../.././media/images/icons/facebook-icon.png")} /></li>
+          <li><img src={require("../../.././media/images/icons/twitter-icon.png")} /></li>
+          <li><img src={require("../../.././media/images/icons/instagram-icon.png")} /></li>
+          </ul>
           <p className="author-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac turpis sed nunc blandit facilisis. Duis sapien est, egestas vitae convallis et, tempor vehicula lectus. Phasellus massa dui, ultrices vel nunc nec, laoreet tincidunt ligula.</p>
 
         </div>
