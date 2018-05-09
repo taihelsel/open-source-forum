b894e6ee719474b34620c9a02f20d7b87d199098
taihelsel <tai.bayard@gmail.com>
2018-05-08 17:32:43 -0700

    added icons to latest stats

--- src/components/SinglePost/SideBar/Explorer/Latest.js
+++ src/components/SinglePost/SideBar/Explorer/Latest.js
@@ -7,30 +7,74 @@ class Latest extends Component {
         <li className="explorer-latest-item">
           <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
           <h1>Lorem Ipsum</h1>
-          <div className="explorer-latest-item-stats">
-            TODO date posted, views, comments.
-              </div>
+          <ul className="explorer-latest-item-stats">
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/clock.png")} />
+              <h3>5/2/2018</h3>
+            </li>
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/eye.png")} />
+              <h3>2.5k</h3>
+            </li>
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/comment.png")} />
+              <h3>58</h3>
+            </li>
+          </ul>
         </li>
         <li className="explorer-latest-item">
           <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
           <h1>Lorem Ipsum</h1>
-          <div className="explorer-latest-item-stats">
-            TODO date posted, views, comments.
-              </div>
+          <ul className="explorer-latest-item-stats">
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/clock.png")} />
+              <h3>5/2/2018</h3>
+            </li>
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/eye.png")} />
+              <h3>2.5k</h3>
+            </li>
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/comment.png")} />
+              <h3>58</h3>
+            </li>
+          </ul>
         </li>
         <li className="explorer-latest-item">
           <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
           <h1>Lorem Ipsum</h1>
-          <div className="explorer-latest-item-stats">
-            TODO date posted, views, comments.
-              </div>
+          <ul className="explorer-latest-item-stats">
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/clock.png")} />
+              <h3>5/2/2018</h3>
+            </li>
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/eye.png")} />
+              <h3>2.5k</h3>
+            </li>
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/comment.png")} />
+              <h3>58</h3>
+            </li>
+          </ul>
         </li>
         <li className="explorer-latest-item">
           <img src={require("../../../.././media/images/stockphotos/calamity.jpg")} />
           <h1>Lorem Ipsum</h1>
-          <div className="explorer-latest-item-stats">
-            TODO date posted, views, comments.
-              </div>
+          <ul className="explorer-latest-item-stats">
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/clock.png")} />
+              <h3>5/2/2018</h3>
+            </li>
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/eye.png")} />
+              <h3>2.5k</h3>
+            </li>
+            <li className="latest-stat-item">
+              <img src={require("../../../.././media/images/icons/comment.png")} />
+              <h3>58</h3>
+            </li>
+          </ul>
         </li>
       </ul>
     );
