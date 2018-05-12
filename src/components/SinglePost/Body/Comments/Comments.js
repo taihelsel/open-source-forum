5fdd6a127ad57758a69305ad9985a9c96aa46814
taihelsel <tai.bayard@gmail.com>
2018-05-12 01:41:48 -0700

    fixed comments component layout

--- src/components/SinglePost/Body/Comments/Comments.js
+++ src/components/SinglePost/Body/Comments/Comments.js
@@ -6,22 +6,22 @@ import SingleComment from "./SingleComment.js";
 class Comments extends Component {
     render() {
         let commentsHeadTxt = "No Comments";
-        if(this.props.comments.length>0){
+        if (this.props.comments.length > 0) {
             commentsHeadTxt = this.props.comments.length + " Comments";
         }
-        return(
-        <div id="Comments">
-            <h1 className="comments-head">
-                {commentsHeadTxt}
-            </h1>
-            <div className="comments-wrapper">
-                {this.props.comments.map((item) => {
-                    return <SingleComment comment={item} />
-                })
-                }
+        return (
+            <div id="Comments">
+                <h1 className="comments-head">
+                    {commentsHeadTxt}
+                </h1>
+                <div className="comments-wrapper">
+                    {this.props.comments.map((item) => {
+                        return <SingleComment comment={item} />
+                    })
+                    }
+                </div>
+                <CommentsForm />
             </div>
-            <CommentsForm />
-        </div>
         );
     }
 }
