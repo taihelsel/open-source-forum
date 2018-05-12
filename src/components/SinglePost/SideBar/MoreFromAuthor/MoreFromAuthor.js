b57ca26a442c135bfdf775a47938144c33ab3e2d
taihelsel <tai.bayard@gmail.com>
2018-05-12 03:51:26 -0700

    rendering more from author component

---
+++ src/components/SinglePost/SideBar/MoreFromAuthor/MoreFromAuthor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MoreFromAuthor extends Component {
+    render() {
+        return (
+            <div id="MoreFromAuthor">
+                test
+            </div>
+        );
+    }
+}
+
+export default MoreFromAuthor;
--- src/components/SinglePost/SideBar/PostSidebar.js
+++ src/components/SinglePost/SideBar/PostSidebar.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 /*ReactComponents*/
 import Explorer from "./Explorer/Explorer.js";
 import Advertisement from "./Advertisement.js";
+import MoreFromAuthor from "./MoreFromAuthor/MoreFromAuthor.js";
 class PostSidebar extends Component {
   render() {
     return (
         <div id="PostSidebar">
           <Explorer />
           <Advertisement adImg={"http://via.placeholder.com/350x150?text=Your+ad+here"}/>
+          <MoreFromAuthor />
         </div>
     );
   }
