966a73849ab3130c4b68eab9a3b4d889607ca6eb
taihelsel <tai.bayard@gmail.com>
2018-05-12 03:08:10 -0700

    rendering sidebar ad component

---
+++ src/components/SinglePost/SideBar/Advertisement.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Advertisement extends Component {
+    render() {
+        return (
+            <div className="sidebar-ad">
+                ad here
+            </div>
+        );
+    }
+}
+
+export default Advertisement;
--- src/components/SinglePost/SideBar/PostSidebar.js
+++ src/components/SinglePost/SideBar/PostSidebar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 /*ReactComponents*/
 import Explorer from "./Explorer/Explorer.js";
+import Advertisement from "./Advertisement.js";
 class PostSidebar extends Component {
   render() {
     return (
         <div id="PostSidebar">
           <Explorer />
+          <Advertisement />
         </div>
     );
   }
