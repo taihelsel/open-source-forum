aae8196e4f9e990d69b7b6d1d4c21016442ca3d5
taihelsel <tai.bayard@gmail.com>
2018-05-12 01:01:48 -0700

    created comments component for singlepost and cleaned up css comments by labeling each different file path*/

---
+++ src/components/SinglePost/Body/Comments.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+    render() {
+        return (
+            <div id="Comments">
+            </div>
+        );
+    }
+}
+
+export default Comments;
