e064dd8accc06d24cf8e966a23a11bf044949e61
taihelsel <tai.bayard@gmail.com>
2018-09-14 18:52:59 -0700

    added slide out effect for sidebar hide

--- client/src/components/CMS/Home/SideNav/SubNav.js
+++ client/src/components/CMS/Home/SideNav/SubNav.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 class SubNav extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            articleSecNav:["New Entry","View All"]
+            articleSecNav: ["New Entry", "View All"]
         }
     }
-    changeNav = (e) =>{
-        if(e.currentTarget.id !== "selected-snav"){
+    changeNav = (e) => {
+        if (e.currentTarget.id !== "selected-snav") {
             document.getElementById("selected-snav").removeAttribute("id");
-            e.currentTarget.setAttribute("id","selected-snav");
+            e.currentTarget.setAttribute("id", "selected-snav");
             this.props.updateSecNav(e.currentTarget.innerText.toLowerCase());
         }
     }
     setInitSelected = (ar) => {
-       return ar.map((item,i)=>{
-            let id=""
-            if(item.toLowerCase() === this.props.currentSecNav) id="selected-snav";
-            return <li key={item+ "-"+i} className="secondary-nav-item" id={id} onClick={this.changeNav}>{item}</li>
+        return ar.map((item, i) => {
+            let id = ""
+            if (item.toLowerCase() === this.props.currentSecNav) id = "selected-snav";
+            return <li key={item + "-" + i} className="secondary-nav-item" id={id} onClick={this.changeNav}>{item}</li>
         })
+    }
+    handleNoShow = () => {
+        if (document.getElementsByClassName("secondary-show")[0]) {
+            return (<ul className="secondary secondary-close"></ul>)
+        }
     }
     render() {
         return this.props.selectedNav === "articles" ? (
-            <ul className="secondary">
+            <ul className="secondary secondary-show">
                 {this.setInitSelected(this.state.articleSecNav)}
             </ul>
         ) : (
-            <div></div>
-        );
+                <ul>{this.handleNoShow()}</ul>
+            )
+            ;
     }
 }
 
